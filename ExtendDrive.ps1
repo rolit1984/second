fa5f8f6813a05f9fedd5ce6db6e088e84ce9e610
Vadzim_Zakruzhny <vadzim_zakruzhny@epam.com>
2017-12-18 16:27:46 +0300

    1

---
+++ ExtendDrive.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.SYNOPSIS
+Script for extend systemdrive on windows server.
+This script use diskpart tool. 
+.DESCRIPTION
+#>
+
+$computername = ""
+$pass = "" | ConvertTo-SecureString -AsPlainText -Force
+$user = "local\Administrator"
+$credential = New-Object Management.Automation.PSCredential $user,$pass
+
+$ss = New-PSSession -Computername $computername -Credential $credential
+Invoke-Command -session $ss  -ScriptBlock {
+
+    # Disk capacity information before extend
+    $diskinfobeforeextend=get-psdrive -Name ($env:systemdrive -replace ":","") -ErrorAction SilentlyContinue
+    $totalcapacity=($diskinfobeforeextend.free + $diskinfobeforeextend.used)/1073741824
+    Write-Host "=========================================================================="
+    Write-Host "Current total capacity $env:systemdrive is $totalcapacity Gb"
+    Write-Host "Current free capacity $env:systemdrive is $($diskinfobeforeextend.free/1073741824) Gb"
+    Write-Host "Current used capacity $env:systemdrive is $($diskinfobeforeextend.used/1073741824) Gb"
+    Write-Host "=========================================================================="
+
+    # Create file and folder for diskpart config file
+    $tempfoldername=get-date -Format ddMMyyyyHHmmss
+    New-Item -Path $env:systemdrive\temp\$tempfoldername -ItemType directory | out-null
+    Set-Content -Path $env:systemdrive\temp\$tempfoldername\diskpartconfig.txt -Force -Value "rescan `r`nselect volume $env:systemdrive `r`nextend"
+
+    # Run diskpart with parameters file and save log in $env:systemdrive\temp\$tempfoldername\log.txt
+    diskpart /s $env:systemdrive\temp\$tempfoldername\diskpartconfig.txt > $env:systemdrive\temp\$tempfoldername\log.txt
+
+    # Translation diskpart log to the console 
+    $log=Get-Content -Path $env:systemdrive\temp\$tempfoldername\log.txt -raw
+    Write-Host $log
+
+    # Disk capacity information after extend
+    $diskinfoafterextend=get-psdrive -Name ($env:systemdrive -replace ":","") -ErrorAction SilentlyContinue
+    $totalcapacity=($diskinfoafterextend.free + $diskinfoafterextend.used)/1073741824
+    Write-Host "=========================================================================="
+    Write-Host "Total capacity (after extend) $env:systemdrive is $totalcapacity Gb"
+    Write-Host "Free capacity (after extend) $env:systemdrive is $($diskinfoafterextend.free/1073741824) Gb"
+    Write-Host "Used capacity (after extend) $env:systemdrive is $($diskinfoafterextend.used/1073741824) Gb"
+    Write-Host "=========================================================================="
+}
+Remove-PSSession $ss
---
+++ FileContentReplace.ps1
@@ -0,0 +1,81 @@
+﻿[CmdletBinding()]
+<#
+.SYNOPSIS 
+Script for replace any content in the text file. 
+.DESCRIPTION
+For remote startup, use Invoke-Command -ComputerName Server01, Server02 -FilePath .\FileContentReplace.ps1 -Credential (Get-Credential)
+Example
+.\FileContentReplace.ps1 -PathToFile D:\test.txt -BackUpPathFile D:\backtest.txt -PatternForReplace "\d" -ReplaceContent "replace"
+#>
+    param
+    (
+        #[Parameter(Mandatory, HelpMessage = "Please enter the path to file.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$PathToFile,
+
+        #[Parameter(Mandatory, HelpMessage = "Please enter the path for backup file.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$BackUpPathFile,
+
+        [Parameter(Mandatory, HelpMessage = "Specify the content that you want to replace the existing content. Do not use quotes.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$ReplaceContent,
+
+        [Parameter(Mandatory, HelpMessage = "Specify the pattern for replace. Do not use quotes.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$PatternForReplace
+        
+    )
+
+    # Check file exists
+    $fileexists=[System.IO.file]::Exists("$PathToFile")
+        if ($fileexists -eq $true) {
+
+            # Get current file content
+            $currentallfilecontent=[System.IO.File]::ReadAllText("$PathToFile")
+            $newfilecontent = $currentallfilecontent -replace $PatternForReplace,$ReplaceContent
+
+            # Create backup in backup location
+            [System.IO.file]::Copy("$PathToFile","$BackUpPathFile")
+            
+            # Check copy process
+            $algorithm = [Security.Cryptography.HashAlgorithm]::Create("MD5")
+            $bytesoriginfile = [io.File]::ReadAllBytes("$PathToFile")
+            $bytesbackfile = [io.File]::ReadAllBytes("$BackUpPathFile")
+            [string]$hashorigin = $algorithm.ComputeHash($bytesoriginfile)
+            [string]$hashbackup = $algorithm.ComputeHash($bytesbackfile)
+
+                if ($hashorigin -eq $hashbackup) {
+                    Write-host "Backup for file $PathToFile created successfully. Path: $BackUpPathFile."
+
+                    # Replace text
+                    [System.IO.file]::WriteAllText("$PathToFile","$newfilecontent")
+
+                    # Check correct replace
+                    $filecontentafterreplace=[System.IO.File]::ReadAllText("$PathToFile")
+                        if ($filecontentafterreplace -eq $newfilecontent) {
+                            Write-Host "Replacement in the file $PathToFile was successfull."
+                            $oldlines=(Compare-Object -ReferenceObject $(Get-Content $PathToFile) -DifferenceObject $(Get-Content $BackUpPathFile) | where {$_.SideIndicator -eq "=>"}).InputObject
+                            $newlines=(Compare-Object -ReferenceObject $(Get-Content $PathToFile) -DifferenceObject $(Get-Content $BackUpPathFile) | where {$_.SideIndicator -eq "<="}).InputObject
+                            Write-Host "Old lines"
+                            Write-Host "--------------------------------"
+                            $oldlines
+                            Write-Host "--------------------------------"
+                            Write-Host "New lines"
+                            Write-Host "--------------------------------"
+                            $newlines
+                            Write-Host "--------------------------------"
+                            }
+                        else {
+                            Write-host "Replacement in the file $PathToFile was failed."
+                            return
+                        }
+                            }
+                else {
+                    Write-host "Create backup of the file $PathToFile failed. Processing stopped."
+                    return
+                }
+            }
+
+
+
