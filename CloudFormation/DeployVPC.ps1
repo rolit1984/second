0d61481acff3eb6bc20f51613987e5332dad9939
Vadim <Vadim>
2017-12-19 15:02:32 +0300

    1

---
+++ CloudFormation/DeployVPC.ps1
@@ -0,0 +1,22 @@
+﻿[CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$AccessKey, 
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$SecretKey
+    )
+
+New-CFNStack -StackName "AutoStack" `
+             -TemplateBody $content `
+             -Parameter @( @{ ParameterKey="GatewayName"; ParameterValue="Gateway"}, @{ ParameterKey="VPCName"; ParameterValue="VPC"}, @{ ParameterKey="PrivateSubnetCIDR"; ParameterValue="172.16.0.0/24"}, @{ ParameterKey="PublicSubnetCIDR"; ParameterValue="172.16.1.0/24"}, @{ ParameterKey="SubnetNamePrefix"; ParameterValue="SubNet"}, @{ ParameterKey="VPCCIDR"; ParameterValue="172.16.0.0/23"}) `
+             -DisableRollback $true -AccessKey $AccessKey -secretkey $SecretKey -region us-east-1
+                    
+
+
+             
+
+
