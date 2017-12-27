16e00df33ab31cc2f001174a50947b532c5af7c8
Vadim <Vadim>
2017-12-27 14:24:27 +0300

    134

---
+++ ACS/RemoveService.ps1
@@ -0,0 +1,28 @@
+﻿[CmdLetBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string] $AzureCloudServiceName
+	)
+
+(Get-AzureDeployment -ServiceName $AzureCloudServiceName -Slot staging).Configuration | out-file d:\stagingconfiguration.xml
+(Get-AzureDeployment -ServiceName $AzureCloudServiceName -Slot production).Configuration | out-file d:\productionconfiguration.xml
+
+
+
+https://management.core.windows.net/b4d05768-e295-4195-9cdb-c07ecd987720/services/hostedservices/zakr/deployments/zakr/package
+
+
+
+
+
+
+
+
+
+
+#New-AzureService -ServiceName "zakr" -Location "East US"
+
+
+
+#New-AzureDeployment -ServiceName zakr -Package D:\tmp\ACS\AzureCloudService5.cspkg -Configuration D:\tmp\ACS\ServiceConfiguration.Cloud.cscfg -Name zakr1 -Label evfrf -slot staging 
--- FileContentReplace.ps1
+++ FileContentReplace.ps1
@@ -2,13 +2,15 @@
 <#
 .SYNOPSIS
 Script for replace any content in the text file.
-.DESCRIPTION
-For remote startup, use Invoke-Command -ComputerName Server01, Server02 -FilePath .\FileContentReplace.ps1 -Credential (Get-Credential)
 Example
 .\FileContentReplace.ps1 -PathToFile D:\test.txt -BackUpPathFile D:\backtest.txt -PatternForReplace "\d" -ReplaceContent "replace"
 #>
     param
     (
+        [Parameter(Mandatory, HelpMessage = "Please enter computer name where file locate")]
+        [ValidateNotNullOrEmpty()]
+        [string]$ComputerName,
+
         [Parameter(Mandatory, HelpMessage = "Please enter the path to file.")]
         [ValidateNotNullOrEmpty()]
         [string]$PathToFile, 
@@ -24,39 +26,40 @@ Example
         [Parameter(Mandatory, HelpMessage = "Specify the pattern for replace. Do not use quotes.")]
         [ValidateNotNullOrEmpty()]
         [string]$PatternForReplace
-        
     )
 
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+
     # Check file exists
-    $fileexists=[System.IO.file]::Exists("$PathToFile")
+    $fileexists=[System.IO.file]::Exists("$using:PathToFile")
         if ($fileexists -eq $true) {
 
             # Get current file content
-            $currentallfilecontent=[System.IO.File]::ReadAllText("$PathToFile")
-            $newfilecontent = $currentallfilecontent -replace $PatternForReplace,$ReplaceContent
+            $currentallfilecontent=[System.IO.File]::ReadAllText("$using:PathToFile")
+            $newfilecontent = $currentallfilecontent -replace $using:PatternForReplace,$using:ReplaceContent
 
             # Create backup in backup location
-            [System.IO.file]::Copy("$PathToFile","$BackUpPathFile")
+            [System.IO.file]::Copy("$using:PathToFile","$using:BackUpPathFile")
             
             # Check copy process
             $algorithm = [Security.Cryptography.HashAlgorithm]::Create("MD5")
-            $bytesoriginfile = [io.File]::ReadAllBytes("$PathToFile")
-            $bytesbackfile = [io.File]::ReadAllBytes("$BackUpPathFile")
+            $bytesoriginfile = [io.File]::ReadAllBytes("$using:PathToFile")
+            $bytesbackfile = [io.File]::ReadAllBytes("$using:BackUpPathFile")
             [string]$hashorigin = $algorithm.ComputeHash($bytesoriginfile)
             [string]$hashbackup = $algorithm.ComputeHash($bytesbackfile)
 
                 if ($hashorigin -eq $hashbackup) {
-                    Write-host "Backup for file $PathToFile created successfully. Path: $BackUpPathFile."
+                    Write-host "Backup for file $using:PathToFile created successfully. Path: $using:BackUpPathFile."
 
                     # Replace text
-                    [System.IO.file]::WriteAllText("$PathToFile","$newfilecontent")
+                    [System.IO.file]::WriteAllText("$using:PathToFile","$newfilecontent")
 
                     # Check correct replace
-                    $filecontentafterreplace=[System.IO.File]::ReadAllText("$PathToFile")
+                    $filecontentafterreplace=[System.IO.File]::ReadAllText("$using:PathToFile")
                         if ($filecontentafterreplace -eq $newfilecontent) {
-                            Write-Host "Replacement in the file $PathToFile was successfull."
-                            $oldlines=(Compare-Object -ReferenceObject $(Get-Content $PathToFile) -DifferenceObject $(Get-Content $BackUpPathFile) | where {$_.SideIndicator -eq "=>"}).InputObject
-                            $newlines=(Compare-Object -ReferenceObject $(Get-Content $PathToFile) -DifferenceObject $(Get-Content $BackUpPathFile) | where {$_.SideIndicator -eq "<="}).InputObject
+                            Write-Host "Replacement in the file $using:PathToFile was successfull."
+                            $oldlines=(Compare-Object -ReferenceObject $(Get-Content $using:PathToFile) -DifferenceObject $(Get-Content $using:BackUpPathFile) | where {$_.SideIndicator -eq "=>"}).InputObject
+                            $newlines=(Compare-Object -ReferenceObject $(Get-Content $using:PathToFile) -DifferenceObject $(Get-Content $using:BackUpPathFile) | where {$_.SideIndicator -eq "<="}).InputObject
                             Write-Host "Old lines"
                             Write-Host "--------------------------------"
                             $oldlines
@@ -65,17 +68,18 @@ Example
                             Write-Host "--------------------------------"
                             $newlines
                             Write-Host "--------------------------------"
-                            }
+                        }
                         else {
-                            Write-host "Replacement in the file $PathToFile was failed."
+                            Write-host "Replacement in the file $using:PathToFile was failed."
                             return
                         }
-                            }
+                }
                 else {
-                    Write-host "Create backup of the file $PathToFile failed. Processing stopped."
+                    Write-host "Create backup of the file $using:PathToFile failed. Processing stopped."
                     return
                 }
-            }
+        }
+    }
 
 
 
