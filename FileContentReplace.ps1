ebdc744aef163aab6659a9eb46c60f1eb5486757
Vadim <Vadim>
2017-12-28 11:41:04 +0300

    ewfwref

--- FileContentReplace.ps1
+++ FileContentReplace.ps1
@@ -34,11 +34,11 @@ Script for replace any content in the text file.
     Invoke-Command -ComputerName $ComputerName -ScriptBlock {
 
     if ($using:KindOfPattern -notlike "regex") {
-        Write-Host "================= Pattern is simple string. Escape special characters if it's necessary ================="
+        Write-Host "========================== Pattern is simple string. Escape special characters if it's necessary ======================="
         $patternforreplace=[regex]::Escape($using:PatternForReplace)
     }
     else {
-        Write-Host "========================== Pattern is regular expression =============================="
+        Write-Host "========================== Pattern is regular expression =========================="
         $patternforreplace=$using:PatternForReplace
     }
 
@@ -50,18 +50,41 @@ Script for replace any content in the text file.
             $currentallfilecontent=[System.IO.File]::ReadAllText("$using:PathToFile")
             $newfilecontent = $currentallfilecontent -replace $patternforreplace,$using:ReplaceContent
 
+            # Update backup file path and check folder exists
+            $backuppathfile=[regex]::split($using:BackUpPathFile, "\\")
+            $pathcounts=0..($backuppathfile.count-1)
+  
+                foreach ($pathcount in $pathcounts) {
+    
+                    if ($pathcount -ne $pathcounts[-1]) {
+                        $backupfullpath=$backupfullpath+$backuppathfile[$($pathcount)]+"\"
+                        $backupfolderpath=$backupfolderpath+$backuppathfile[$($pathcount)]+"\"   
+                    }
+                    else {
+                        $backupfullpath=$backupfullpath+(get-date -format ddMMyyyyHHmmss)+"-"+$backuppathfile[$($pathcount)]
+                    }  
+                }
+            Write-Host "========================== BackUp folder path: $backupfolderpath =========================="
+            Write-Host "========================== BackUp full path: $backupfullpath =========================="
+            $checkbackupdirectory=[System.IO.Directory]::Exists("$backupfolderpath")
+                if ($checkbackupdirectory -ne $true) {
+                    Write-Host "========================== BackUp folder does not exist. Creating ... =========================="
+                    [system.io.directory]::CreateDirectory("$backupfolderpath") | Out-Null
+                }
+                    
+
             # Create backup in backup location
-            [System.IO.file]::Copy("$using:PathToFile","$using:BackUpPathFile")
+            [System.IO.file]::Copy("$using:PathToFile","$backupfullpath")
             
             # Check copy process
             $algorithm = [Security.Cryptography.HashAlgorithm]::Create("MD5")
             $bytesoriginfile = [io.File]::ReadAllBytes("$using:PathToFile")
-            $bytesbackfile = [io.File]::ReadAllBytes("$using:BackUpPathFile")
+            $bytesbackfile = [io.File]::ReadAllBytes("$backupfullpath")
             [string]$hashorigin = $algorithm.ComputeHash($bytesoriginfile)
             [string]$hashbackup = $algorithm.ComputeHash($bytesbackfile)
 
                 if ($hashorigin -eq $hashbackup) {
-                    Write-host "Backup for file $using:PathToFile created successfully. Path: $using:BackUpPathFile."
+                    Write-host "========================== Backup for file $using:PathToFile created successfully. Path: $backupfullpath. =========================="
 
                     # Replace text
                     [System.IO.file]::WriteAllText("$using:PathToFile","$newfilecontent")
@@ -69,17 +92,13 @@ Script for replace any content in the text file.
                     # Check correct replace
                     $filecontentafterreplace=[System.IO.File]::ReadAllText("$using:PathToFile")
                         if ($filecontentafterreplace -eq $newfilecontent) {
-                            Write-Host "Replacement in the file $using:PathToFile was successfull."
-                            $oldlines=(Compare-Object -ReferenceObject $(Get-Content $using:PathToFile) -DifferenceObject $(Get-Content $using:BackUpPathFile) | where {$_.SideIndicator -eq "=>"}).InputObject
-                            $newlines=(Compare-Object -ReferenceObject $(Get-Content $using:PathToFile) -DifferenceObject $(Get-Content $using:BackUpPathFile) | where {$_.SideIndicator -eq "<="}).InputObject
-                            Write-Host "Old lines"
-                            Write-Host "--------------------------------"
+                            Write-Host "========================== Replacement in the file $using:PathToFile was successfull. =========================="
+                            $oldlines=(Compare-Object -ReferenceObject $(Get-Content $using:PathToFile) -DifferenceObject $(Get-Content $backupfullpath) | where {$_.SideIndicator -eq "=>"}).InputObject
+                            $newlines=(Compare-Object -ReferenceObject $(Get-Content $using:PathToFile) -DifferenceObject $(Get-Content $backupfullpath) | where {$_.SideIndicator -eq "<="}).InputObject
+                            Write-Host "========================== Old lines =========================="
                             $oldlines
-                            Write-Host "--------------------------------"
-                            Write-Host "New lines"
-                            Write-Host "--------------------------------"
+                            Write-Host "========================== New lines =========================="
                             $newlines
-                            Write-Host "--------------------------------"
                         }
                         else {
                             Write-host "Replacement in the file $using:PathToFile was failed."
